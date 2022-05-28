4e5e31897758fa8697739ae57b6382f384596e44
Guillaume MORIN <guillaume.morin@epitech.eu>
2022-05-28 15:50:27 +0400

    end

---
+++ src/components/Timer.tsx
@@ -0,0 +1,60 @@
+
+  
+import { FC, useEffect, useState } from "react";
+
+const Timer:FC = () => {
+  const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("11/04/2022 23:59:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {!partyTime && (
+        <>
+            <div className="text-center my-9 text-2xl">
+                <span className="">{days} jours</span>
+                <br />
+                <span className="">{hours} heures</span>
+                <br />
+                <span className="">{minutes} minutes</span>
+                <br />
+                <span className="">{seconds} secondes</span>
+            </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Timer;
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,30 +1,37 @@
 import { FC } from "react";
 import Mac from '../assets/macbook.png'
 import M1 from '../assets/m1.png'
+import Winamax from '../assets/winamax.png'
+import Timer from "../components/Timer";
 
 const Home: FC = () => {
-
     return (
-        <div className="h-min-screen w-full bg-black text-white">
+        <div className="min-h-screen w-full bg-black text-white font-mono">
             <div className="grid"/>
-            <div className="grid grid-cols-9 mx-auto text-center">
-                <div className="grid lg:grid-cols-2 grid-cols-1 md:col-span-4 col-span-9 mx-auto text-center h-screen">
-                    <div className="flex items-center justify-center">
+            <h1 className="font-mono text-center mt-12 text-2xl md:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
+                Compte à rebours (3 novembre 2022):
+            </h1>
+            <Timer />
+            <div className="flex justify-center text-center flex-col md:flex-row md:items-center mt-15">
+                <div className="flex justify-center flex-col md:flex-row items-center mx-auto text-center">
+                    <div className="">
                         <img src={Mac} className="object-scale-down h-50 md:h-96" alt="mac" />
+                        <p>Prix: 2 200€</p>
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="">
                         <img src={M1} className="object-scale-down h-50 md:h-60" alt="mac" />
                     </div>
                 </div>
-                <div className="h-screen flex items-center col-span-1">
-                    <div className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 w-0 md:w-1 h-1/5 mx-auto" />
+                <div className="flex items-center h-0">
+                    <div className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 w-0 md:w-1 h-0 md:h-2/5 mx-auto" />
                 </div>
-                <div className="flex md:col-span-4 col-span-9 items-center grid-cols-1 mx-auto text-center h-screen">
+                <div className="mx-auto text-center flex justify-center items-center">
                     <div>
-                        <h1 className="mb-5 font-mono text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r hover:from-green-400 hover:to-blue-500 from-pink-500 to-yellow-500">
+                        <img src={Winamax} className="object-scale-down h-24 md:h-24 mx-auto mb-5" alt="mac" />
+                        <h1 className="mb-5 font-mono text-2xl md:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r hover:from-green-400 hover:to-blue-500 from-pink-500 to-yellow-500">
                             Somme récoltée:
                         </h1>
-                        <h1 className="font-mono text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
+                        <h1 className="font-mono text-2xl md:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
                             250€
                         </h1>
                     </div>
